55912121d448f929dbd7246755b9830c38a5abed
WIN-RKVG7LDFOI3\Vitalka <berginion1@gmail.com>
2016-11-14 20:13:31 +0400

    Правки Девайс

--- js/script.js
+++ js/script.js
@@ -1,14 +1,91 @@
-function checkParams() {
-    var search = $('#search').val();
-    if (search.length = 0) {
-        $('#search-btn').attr('hidden');
+var letter = document.querySelector (".letter");
+  
+var popup = document.querySelector (".popup-container");
+var overlay = document.querySelector (".popup-overlay");
+var close = popup.querySelector (".popup-container-close");
+  
+var form = popup.querySelector (".popup-form");
+var username = popup.querySelector ("[name=user-name]");
+var email = popup.querySelector ("[name=email]");
+var message = popup.querySelector ("[name=message-text]");
+
+var storage = localStorage.getItem ("username");
+
+letter.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("popup-container-display");
+  overlay.classList.add("popup-overlay-display");
+  username.focus();
+    
+  if (storage) {
+    username.value = storage;
+      email.focus();
     } else {
-        $('#search-btn').removeAttr('hidden');
+    username.focus();
     }
-} 
+});
 
-$('div.main-menu li').each(function () {
-    if (this.getElementsByTagName("a")[0].href == location.href) this.className = "selected";});
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("popup-container-display");
+  overlay.classList.remove("popup-overlay-display");
+});
+
+overlay.addEventListener("click", function(event) {
+  if (popup.classList.contains("popup-container-display")) {
+    popup.classList.remove("popup-container-display");
+    overlay.classList.remove("popup-overlay-display");
+  }
+});
+
+form.addEventListener("submit", function(event) {
+  if (!username.value || !email.value || !message.value) {
+    event.preventDefault();
+  } else {
+    localStorage.setItem("username", username.value);
+  }
+});
+
+window.addEventListener ("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("popup-container-display")) {
+      popup.classList.remove("popup-container-display");
+      overlay.classList.remove("popup-overlay-display");
+    }
+  }
+});
 
 
 
+var map = document.querySelector (".map");
+var popupmap = document.querySelector (".popup-map");
+var mapclose = popupmap.querySelector (".popup-container-close");
+
+map.addEventListener("click", function(event) {
+  event.preventDefault();
+  popupmap.classList.add("popup-map-display");
+  overlay.classList.add("popup-overlay-display");
+});
+
+mapclose.addEventListener("click", function(event) {
+  event.preventDefault();
+  popupmap.classList.remove("popup-map-display");
+  overlay.classList.remove("popup-overlay-display");
+});
+
+overlay.addEventListener("click", function(event) {
+  if (popupmap.classList.contains("popup-map-display")) {
+    popupmap.classList.remove("popup-map-display");
+    overlay.classList.remove("popup-overlay-display");
+  }
+});
+
+window.addEventListener ("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popupmap.classList.contains("popup-map-display")) {
+      popupmap.classList.remove("popup-map-display");
+      overlay.classList.remove("popup-overlay-display");
+    }
+  }
+});
+  
