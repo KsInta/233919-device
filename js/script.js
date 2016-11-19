8c91db3d036aaef2f23974c05c1a0e9e35542065
WIN-RKVG7LDFOI3\Vitalka <berginion1@gmail.com>
2016-11-19 10:33:30 +0400

    на финальную

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 var letter = document.querySelector(".letter");
-  
 var popup = document.querySelector(".popup-container");
-var overlay = document.querySelector(".popup-overlay");
 var close = popup.querySelector(".popup-container-close");
   
 var form = popup.querySelector(".popup-form");
@@ -13,25 +11,16 @@ var map = document.querySelector (".map");
 var popupmap = document.querySelector (".popup-map");
 var mapclose = popupmap.querySelector (".popup-container-close");
 
-//Модальное окно
+/*Модальное окно*/
 letter.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.add("popup-container-display");
-  overlay.classList.add("popup-overlay-display");
   username.focus();
 });
 
 close.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.remove("popup-container-display");
-  overlay.classList.remove("popup-overlay-display");
-});
-
-overlay.addEventListener("click", function(event) {
-  if (popup.classList.contains("popup-container-display")) {
-    popup.classList.remove("popup-container-display");
-    overlay.classList.remove("popup-overlay-display");
-  }
 });
 
 form.addEventListener("submit", function(event) {
@@ -40,41 +29,26 @@ form.addEventListener("submit", function(event) {
   } 
 });
 
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (popup.classList.contains("popup-container-display")) {
-      popup.classList.remove("popup-container-display");
-      overlay.classList.remove("popup-overlay-display");
-    }
-  }
-});
-
-//Карта
+/*Карта*/
 map.addEventListener("click", function(event) {
   event.preventDefault();
   popupmap.classList.add("popup-map-display");
-  overlay.classList.add("popup-overlay-display");
 });
 
 mapclose.addEventListener("click", function(event) {
   event.preventDefault();
   popupmap.classList.remove("popup-map-display");
-  overlay.classList.remove("popup-overlay-display");
-});
-
-overlay.addEventListener("click", function(event) {
-  if (popupmap.classList.contains("popup-map-display")) {
-    popupmap.classList.remove("popup-map-display");
-    overlay.classList.remove("popup-overlay-display");
-  }
 });
 
 window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
+  if (event.keyCode === 27) {   
+    if (popup.classList.contains("popup-container-display")) {
+      popup.classList.remove("popup-container-display");
+    }
     if (popupmap.classList.contains("popup-map-display")) {
       popupmap.classList.remove("popup-map-display");
-      overlay.classList.remove("popup-overlay-display");
     }
   }
 });
+
   
